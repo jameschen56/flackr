ef1a075fdd165226a584a9c97774af3cace49879
jameschen56 <jameschen56@gmail.com>
2022-02-06 17:59:31 -0800

    start working on splash page

---
+++ frontend/src/components/SplashPage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import InspirationContent from './InspirationContent';
+
+const SplashPage = () => {
+
+    return (
+        <div className = "splash-content">
+            <div className ="splash-slideshow">
+                <div className="splash-inspiration">
+                    {/* <InspirationContent /> */}
+                    <div className="splash-attribution">
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SplashPage;
