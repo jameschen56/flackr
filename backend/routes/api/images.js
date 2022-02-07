a78e9767d8bf33936e7d0a438e11dcd02708446a
jameschen56 <jameschen56@gmail.com>
2022-02-07 11:14:21 -0800

    working on backend routes for images

---
+++ backend/routes/api/images.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+const router = express.Router();
+
+const { Image } = require('../../db/models')
+
+router.get('', asyncHandler(async (req, res) => {
+    const images = await Image.findAll();
+    // console.log('images', images)
+    res.json(images)
+}))
+
+
+module.exports = router;
