2cc9e1e78ae31bbc303b8c1e6189dd104ad337f6
jameschen56 <jameschen56@gmail.com>
2022-02-07 06:49:08 -0800

    remove background image and need to fix image issue

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import InspirationWords from './InspirationWords';
+import React from "react";
+import "./SplashPage.css"
+// import InspirationWords from "./InspirationWords";
 
 const SplashPage = () => {
-
-    return (
-        <div className = "splash-content">
-            <div className ="splash-slideshow">
-                <div className="splash-inspiration">
-                    <InspirationWords />
-                    <div className="splash-attribution">
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="splash-container">
+      <div className="splash-text-container">
+        <h2 className="header1">Find your inspiration.</h2>
+        <h5 className="slogan">Join the Flackr community, home to tens of billions of photos and 2 million groups.</h5>
+      </div>
+      <footer className="network-links">
+        <div>James Chen</div>
+        <a
+          href="https://www.linkedin.com/in/james-chen56/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fab fa-linkedin fa-lg"></i>
+        </a>
+        <a
+          href="https://github.com/jameschen56/flackr"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fab fa-github fa-lg"></i>
+        </a>
+      </footer>
+    </div>
+  );
+};
 
 export default SplashPage;
