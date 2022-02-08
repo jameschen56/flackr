e7e8c34e4c6139fb8bc227ed68860514f642fa57
jameschen56 <jameschen56@gmail.com>
2022-02-07 16:44:11 -0800

    working on get images

--- frontend/src/components/PhotoStream/index.js
+++ frontend/src/components/PhotoStream/index.js
@@ -1,31 +1,31 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getAllImages } from "../../store/image"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllImages } from "../../store/image";
 // import ImageDetail from '../ImageDetail'
 // import SingleImage from '../SingleImage'
 
 const PhotoStream = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const imagesObj = useSelector((state) => (state.image.entries))
-    // console.log('imagesObj', imagesObj)
-    // const images = Object.values(imagesObj)
+  const images = useSelector((state) => state.image);
+  console.log('images', images)
+  const imagesArr = Object.values(images);
+  console.log("imagesArr", imagesArr);
 
-    useEffect(() => {
-        dispatch(getAllImages())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getAllImages());
+  }, [dispatch]);
 
-    return (
-    
-        // <div className="photo-stream">
-        //      {sessionImages?.map(({ imageUrl, id, description }) => (
-        //             <ImageDetail key={id} id={id} imageUrl={imageUrl} description={description}/>
-        //         ))}
-        // </div>
-        <h1>PhotoStream</h1>
-        
-    )
-}
-
+  return (
+    <div>
+      <h1>PhotoStream</h1>
+      <ol>
+        {imagesArr?.map((image) => (
+          <l1 key={image.id}>{image.description}</l1>
+        ))}
+      </ol>
+    </div>
+  );
+};
 
 export default PhotoStream;
