079d5f52723ab892b0e36afe4951992722e18414
jameschen56 <jameschen56@gmail.com>
2022-02-11 07:38:44 -0800

    troubleshooting update comments bug

---
+++ frontend/src/components/Comments/index.js
@@ -0,0 +1,97 @@
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useParams, Redirect, Link} from 'react-router-dom';
+import EditCommentModal from './EditCommentModal';
+import {getComments, createComment,removeComment} from '../../store/comment';
+
+const Comment = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [comment, setComment] = useState('');
+    const [commentToDeleteId, setCommentToDeleteId] = useState('');
+
+
+    const {id} = useParams()
+
+    const comments = useSelector(state=>{
+        return Object.values(state.comments)
+    })
+
+    console.log('comments', comments)
+
+    const usersComments = comments.filter(comment => comment.imageId === Number(id))
+
+    console.log('userComments', usersComments)
+
+    useEffect(() => {
+        dispatch(getComments(id))
+    }, [dispatch, id])
+
+    if(!comments) return null;
+
+    if(!sessionUser) {
+        return <Redirect to='/login' />
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const commentData = {
+            comment: comment,
+            userId: sessionUser.id,
+            imageId: id
+        }
+
+        await dispatch(createComment(commentData))
+        setComment('')
+    }
+
+    const handleDeleteComment = async (e) => {
+        e.preventDefault();
+        await dispatch(removeComment(commentToDeleteId))
+    }
+
+
+
+    return (
+        <div className='comment-container'>
+            {usersComments.length === 0 ? <div className="empty-comment">Post your comment here!</div> :
+            usersComments?.map((comment)=>(
+                <div class="comments-container">
+                    <h4><Link className="link-comment-user" to={`/users/${comment.User.userId}`}>{comment.User?.username}</Link></h4>
+                    <div className="user-comment-info">
+                        <div className="comment-p">
+                            <p>{comment.comment}</p>
+                        </div>
+                        <div className="user-comment-delete-edit">
+                            <div>
+                                <form onSubmit={handleDeleteComment} hidden={comment.userId !== sessionUser.id}>
+                                    <button className="fas fa-times" text="Delete Comment" onClick={e=> setCommentToDeleteId(comment.id)}></button>
+                                </form>
+                            </div>
+                            <div hidden={comment.userId !== sessionUser.id} className="edit-comment">
+                                <EditCommentModal />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))
+
+            }
+
+
+            <div className="comment-form-container">
+                <form className="comment-form" onSubmit={handleSubmit}>
+                    <div className="comment-textarea">
+                        <textarea placeholder='Add a comment' value={comment} onChange={(e) => setComment(e.target.value)} required></textarea>
+                    </div>
+                    <div className='post-comment-bttn-div'>
+                        <button className='post-comment-bttn' onClick={handleSubmit}>Post Comment</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Comment
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import imageReducer from "./image";
 import sessionReducer from "./session";
+import commentReducer from './comment';
 
 const rootReducer = combineReducers({
     session: sessionReducer,
     image: imageReducer,
+    comments: commentReducer,
 });
 
 let enhancer;
