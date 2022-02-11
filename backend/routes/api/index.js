ac5dd2f93744422a72edc499ce3eefe79a68f371
jameschen56 <jameschen56@gmail.com>
2022-02-10 18:33:41 -0800

    finished backend GET route for comment

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const imagesRouter = require('./images')
+const commentsRouter = require('./comments')
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
 router.use('/images', imagesRouter);
+
+router.use('/comments', commentsRouter);
 
 module.exports = router;
