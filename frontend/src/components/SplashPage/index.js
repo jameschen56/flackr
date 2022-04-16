5e34f2b620c9202faac60379e9e853b2208a2021
jameschen56 <jameschen56@gmail.com>
2022-04-16 06:52:40 -0700

    updated css styling for login page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -20,6 +20,8 @@ const SplashPage = () => {
                 <li><span>Image 01</span></li>
                 <li><span>Image 02</span></li>
                 <li><span>Image 03</span></li>
+                <li><span>Image 04</span></li>
+                <li><span>Image 05</span></li>
             </ul>
       <Footer />
     </>
